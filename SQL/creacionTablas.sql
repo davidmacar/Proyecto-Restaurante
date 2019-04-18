34b544f831987ef0ca5b157ee1f955451096f154
braisrodriguez14 <49751466+braisrodriguez14@users.noreply.github.com>
2019-04-18 15:29:12 +0200

    Add files via upload

---
+++ SQL/creacionTablas.sql
@@ -0,0 +1,129 @@
+﻿create table personal (
+	dni varchar(9) primary key,
+	salario numeric not null check (salario > 0),
+	numero_tlf integer,
+	fecha_entrada time,
+	fecha_salida time,
+	nombre varchar(30) not null,
+	apellido_1 varchar(30) not null,
+	apellido_2 varchar(30) not null,
+	e_mail varchar(50)
+);
+
+create table gerente (
+	codigo_trabajador varchar(9) primary key,
+	foreign key (codigo_trabajador) references personal(dni)
+		on delete cascade on update cascade
+);
+
+create table cocinero (
+	codigo_trabajador varchar(9) primary key,
+	foreign key (codigo_trabajador) references personal(dni)
+		on delete cascade on update cascade
+);
+
+create table camarero (
+	codigo_trabajador varchar(9) primary key,
+	foreign key (codigo_trabajador) references personal(dni)
+		on delete cascade on update cascade
+);
+
+create table cajero (
+	codigo_trabajador varchar(9) primary key,
+	foreign key (codigo_trabajador) references personal(dni)
+		on delete cascade on update cascade
+);
+
+create table mesas (
+	num_mesa integer unique,
+	cajero varchar(9),
+	camarero varchar(9),
+	id_venta integer unique,
+	precio numeric,
+	fecha_venta timestamp,
+	primary key (num_mesa, id_venta),
+	foreign key (cajero) references cajero(codigo_trabajador)
+		on delete cascade on update cascade,
+	foreign key (camarero) references camarero(codigo_trabajador)
+		on delete cascade on update cascade
+);
+
+create table materias_primas (
+	nombre varchar(40) primary key,
+	cantidad integer check (cantidad >= 0),
+	precio numeric not null check (precio >= 0),
+	gerente varchar(9),
+	foreign key (gerente) references gerente(codigo_trabajador)
+		on delete cascade on update cascade
+);
+
+create table bebidas (
+	nombre varchar(40) primary key,
+	mesa integer,
+	id_venta integer,
+	foreign key (nombre) references materias_primas(nombre)
+		on delete cascade on update cascade,
+	foreign key (mesa) references mesas(num_mesa)
+		on delete cascade on update cascade,
+	foreign key (id_venta) references mesas(id_venta)
+		on delete cascade on update cascade
+);
+
+create table ingredientes (
+	nombre varchar(40) primary key,
+	foreign key (nombre) references materias_primas(nombre)
+		on delete cascade on update cascade
+);
+
+create table platos (
+	id_plato integer primary key,
+	apto_veganos boolean not null,
+	apto_celiacos boolean not null,
+	tipo varchar(40),
+	descripcion varchar(100),
+	mesa integer,
+	id_venta integer,
+	foreign key (mesa) references mesas(num_mesa)
+		on delete cascade on update cascade,
+	foreign key (id_venta) references mesas(id_venta)
+		on delete cascade on update cascade
+);
+
+create table ingrediente_forma_plato (
+	plato integer,
+	ingrediente varchar(40),
+	primary key (plato, ingrediente),
+	foreign key (plato) references platos(id_plato)
+		on delete cascade on update cascade,
+	foreign key (ingrediente) references materias_primas(nombre)
+		on delete cascade on update cascade
+);
+
+create table cocinar_plato (
+	plato integer,
+	cocinero varchar(9),
+	primary key (plato, cocinero),
+	foreign key (plato) references platos(id_plato)
+		on delete cascade on update cascade,
+	foreign key (cocinero) references cocinero(codigo_trabajador)
+		on delete cascade on update cascade
+);
+
+create table empresa_marketing (
+	nombre varchar(50) unique,
+	gerente varchar(9),
+	precio numeric,
+	primary key (nombre, gerente),
+	foreign key (gerente) references gerente(codigo_trabajador)
+		on delete cascade on update cascade
+);
+
+create table campaña_publicitaria(
+	empresa varchar(50),
+	fecha_inicio timestamp,
+	fecha_fin timestamp,
+	primary key (empresa, fecha_inicio, fecha_fin),
+	foreign key (empresa) references empresa_marketing(nombre)
+		on delete cascade on update cascade
+
+);
---
+++ SQL/insercionDatos.sql
@@ -0,0 +1,24 @@
+﻿-- Personal
+insert into personal values ('11111111A', '1000.5', '654654654', '9:00:00', '10:00:00', 'A', 'B', 'C', 'a@mail.com');
+insert into personal values ('22222222B', '1000.5', '654654654', '9:00:00', '10:00:00', 'A', 'B', 'C', 'a@mail.com');
+insert into personal values ('33333333C', '1000.5', '654654654', '9:00:00', '10:00:00', 'A', 'B', 'C', 'a@mail.com');
+insert into personal values ('44444444D', '1000.5', '654654654', '9:00:00', '10:00:00', 'A', 'B', 'C', 'a@mail.com');
+
+-- Gerente
+insert into gerente values ('11111111A');
+
+-- Cocinero
+insert into cocinero values ('22222222B');
+
+-- Camarero
+insert into camarero values ('33333333C');
+
+-- Cajero
+insert into cajero values ('44444444D');
+
+-- Mesas
+insert into mesas values ('1', '44444444D', '33333333C', '2', '10.6', '2018-04-10 15:00:00');
+
+-- Materias primas
+insert into materias_primas values ('tomate', '5', '1.5', '11111111A');
+
